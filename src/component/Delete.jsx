2e9893b5e00ab828be4ccdd93258c57eb461ef46
davy Thiebaut <davy.du.60@hotmail.fr>
2026-06-15 17:47:56 +0200

    maj

---
+++ src/component/Delete.jsx
@@ -0,0 +1,32 @@
+import { Button, Modal } from 'react-bootstrap';
+
+function Delete({ showDelete, onClose }) {
+    return (
+        <Modal
+            show={showDelete}
+            centered
+            onHide={onClose}
+            data-bs-theme="dark"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Suppression</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                Es-tu sûr de vouloir supprimer cette recette ?
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onClose}>
+                    Annuler
+                </Button>
+
+                <Button variant="danger">
+                    Supprimer
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default Delete;
